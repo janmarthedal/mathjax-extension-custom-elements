53f7edaf93f59397a46c54c14f5c9850f1e5546a
Jan Rasmussen <JMRasmussen@kapowsoftware.com>
2014-07-08 14:20:20 +0200

    Re-typeset automatically using MutationObserver

--- unpacked/math-tex-element.js
+++ unpacked/math-tex-element.js
@@ -1,9 +1,36 @@
 (function() {
 
+    var observeOptions = {attributes: true, childList: true, characterData: true, subtree: true};
     var ready = MathJax.isReady, localQueue = [];
 
-    function enqueueTypeset(el) {
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            var elem;
+            if (mutation.type === 'attributes')
+                elem = mutation.target;
+            else if (mutation.type === 'characterData')
+                elem = mutation.target.parentNode;
+            else if (mutation.type === 'childList' && mutation.addedNodes.length)
+                elem = mutation.addedNodes[0].parentNode;
+            if (elem && elem._jaxScript) {
+                updateShadowDOM(elem);
+                MathJax.Hub.Queue(["Reprocess", MathJax.Hub, elem._jaxScript]);
+            }
+        });
+    });
+
+    function updateShadowDOM(elem) {
+        elem._jaxScript.type = "math/tex";
+        if (elem.getAttribute('mode') === 'display') {
+            elem._jaxScript.type += "; mode=display";
+        }
+        elem._jaxScript.textContent = elem.textContent;
+    }
+
+    function enqueueTypeset(elem) {
+        updateShadowDOM(elem);
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, elem._jaxScript]);
+        observer.observe(elem, observeOptions);
     }
 
     if (!ready) {
@@ -19,16 +46,15 @@
 
     var MathTexProto = Object.create(HTMLElement.prototype);
 
-    MathTexProto.attachedCallback = function () {
+    MathTexProto.createdCallback = function () {
         var shadow = this.createShadowRoot();
-        var jaxelem = document.createElement('script');
-        jaxelem.type = "math/tex";
-        if (this.getAttribute('mode') === 'display') {
-          jaxelem.type += "; mode=display";
-        }
-        jaxelem.innerHTML = this.textContent;
-        shadow.appendChild(jaxelem);
-        ready ? enqueueTypeset(jaxelem) : localQueue.push(jaxelem);
+        var script = document.createElement('script');
+        shadow.appendChild(script);
+        this._jaxScript = script;
+    };
+
+    MathTexProto.attachedCallback = function () {
+        ready ? enqueueTypeset(this) : localQueue.push(this);
     };
 
     document.registerElement('math-tex', {prototype: MathTexProto});
